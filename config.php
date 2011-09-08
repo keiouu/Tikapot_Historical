a9b3a2d17b36a7cb5ad116db44850b4eb2de191b
James Thompson <keiouu@gmail.com>
2011-09-08 23:14:27 +0100

    Added admin and dev portal init scripts

--- config.php
+++ config.php
@@ -14,7 +14,7 @@ $database_username = "tikapot";
 $database_password = "tikapot";
 
 $app_paths = array("apps", "contrib");
-$apps_list = array("example");
+$apps_list = array("admin", "example", "development");
 
 ?>
 
---
+++ contrib/development/init.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Tikapot Developer Portal Init Script
+ *
+ * Copyright 2011, AUTHORS.txt
+ * Licensed under the GNU General Public License version 3.
+ * See LICENSE.txt
+ */
+ 
+global $home_dir;
+include_once($home_dir . "apps/example/models.php");
+require_once($home_dir . "apps/example/views.php");
+require_once($home_dir . "framework/view.php");
+
+
+?>
