3ae6f560d2f3d563d9473bc1333d87266eb54a04
Keiouu <keiouu@gmail.com>
2011-09-09 15:20:20 +0100

    More accurate timing on index

--- index.php
+++ index.php
@@ -7,19 +7,20 @@
  * See LICENSE.txt
  */
 
+$time = microtime(True);
+
 ini_set('display_errors', '1');
 
 $home_dir = dirname(__FILE__) . '/';
-require_once($home_dir . "contrib/timer.php");
-$pageloadtimer = Timer::start();
 require_once($home_dir . "config.php");
 require_once($home_dir . "framework/view_manager.php");
 require_once($home_dir . "framework/app_loader.php");
 require_once($home_dir . "framework/request.php");
+require_once($home_dir . "contrib/timer.php");
 
 $view_manager = new ViewManager();
 load_applications();
-$request = new Request($pageloadtimer);
+$request = new Request(Timer::startAt($time));
 $view_manager->get($request->page)->render($request);
 
 ?>
