7616916160504d12929487f0e8dd67c7ae43705a
Keiouu <keiouu@gmail.com>
2011-09-09 15:16:15 +0100

    Little things..

--- index.php
+++ index.php
@@ -10,11 +10,10 @@
 ini_set('display_errors', '1');
 
 $home_dir = dirname(__FILE__) . '/';
-
-require_once("config.php");
-require_once("framework/view_manager.php");
-require_once("framework/app_loader.php");
-require_once("framework/request.php");
+require_once($home_dir . "config.php");
+require_once($home_dir . "framework/view_manager.php");
+require_once($home_dir . "framework/app_loader.php");
+require_once($home_dir . "framework/request.php");
 
 $view_manager = new ViewManager();
 load_applications();
