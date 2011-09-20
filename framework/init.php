8f298ed16760539f450101bc1461753d4b5f5d8f
Keiouu <keiouu@gmail.com>
2011-09-20 12:32:25 +0100

    Comments! :o

--- framework/init.php
+++ framework/init.php
@@ -15,26 +15,33 @@
 
 $time = microtime(True);
 
+/* Start up the signal manager, register some signals */
 require_once(home_dir . "framework/signal_manager.php");
 $signal_manager = new SignalManager();
 $signal_manager->register("page_load_start");
 $signal_manager->register("page_load_end");
 
+/* Start up the view manager */
 require_once(home_dir . "framework/view_manager.php");
 $view_manager = new ViewManager();
 
+/* Load the apps */
 require_once(home_dir . "framework/app_loader.php");
 load_applications();
 
+/* Create the request */
 require_once(home_dir . "contrib/timer/timer.php");
 require_once(home_dir . "framework/request.php");
 $request = new Request(Timer::startAt($time));
 
+/* Fire page start event */
 $signal_manager->fire("page_load_start", $request);
 
 header('Content-type: ' . $request->mimeType);
 
+/* Render the page */
 $view_manager->get($request->page)->render($request);
 
+/* Fire page end event */
 $signal_manager->fire("page_load_end", $request);
 ?>
